d8be001c43d0a7fe9459100c4a9441b992f2bf74
Siubeng Fung <fython@163.com>
2020-07-15 21:05:52 +0800

    Use ioredis instead of node-redis package
    Signed-off-by: Siubeng Fung <fython@163.com>

--- bot/statistics.js
+++ bot/statistics.js
@@ -1,31 +1,18 @@
-const { promisify } = require('util');
-const redis = require('redis');
+const Redis = require('ioredis');
 
 class DanmakuStatistics {
     constructor(botConfig, logger) {
         this.enabled = botConfig.statistics.enabled;
         let redisServer = botConfig.statistics.redisServer;
-        if (!redisServer.startsWith('//')) {
-            redisServer = '//' + redisServer;
+        if (!redisServer.startsWith('redis://') && !redisServer.startsWith('rediss://')) {
+            redisServer = 'redis://' + redisServer;
         }
         if (!this.enabled) return;
-        const client = redis.createClient(redisServer);
-        this.client = client;
+        this.client = new Redis(redisServer);
         this.logger = logger;
         this.selectDBIndex = botConfig.statistics.selectDB;
 
-        this.getSync = promisify(client.get).bind(client);
-        this.incrSync = promisify(client.incr).bind(client);
-        this.incrbySync = promisify(client.incrby).bind(client);
-        this.selectSync = promisify(client.select).bind(client);
-        this.keysSync = promisify(client.keys).bind(client);
-        this.saddSync = promisify(client.sadd).bind(client);
-        this.smembersSync = promisify(client.smembers).bind(client);
-
         this._selectDanmaquaDB().catch((e) => {
-            this.logger.default.error(e);
-        });
-        client.on('error', (e) => {
             this.logger.default.error(e);
         });
     }
@@ -33,83 +20,83 @@ class DanmakuStatistics {
     async _selectDanmaquaDB() {
         let dbIndex = this.selectDBIndex;
         if (isNaN(this.selectDBIndex)) {
-            dbIndex = await this.getSync('danmaqua:db_index');
+            dbIndex = await this.client.get('danmaqua:db_index');
         }
         if (dbIndex > 0) {
-            await this.selectSync(dbIndex);
+            await this.client.select(dbIndex);
         }
     }
 
     async incrementSentences(userId, roomId) {
         if (!this.enabled) return;
-        await this.saddSync('users', userId);
-        await this.saddSync('rooms', roomId);
-        return await this.incrSync(`sentences:${userId}:${roomId}`);
+        await this.client.sadd('users', userId);
+        await this.client.sadd('rooms', roomId);
+        return await this.client.incr(`sentences:${userId}:${roomId}`);
     }
 
     async incrementWordsBy(userId, roomId, count) {
         if (!this.enabled) return;
-        await this.saddSync('users', userId);
-        await this.saddSync('rooms', roomId);
-        return await this.incrbySync(`words:${userId}:${roomId}`, count);
+        await this.client.sadd('users', userId);
+        await this.client.sadd('rooms', roomId);
+        return await this.client.incrby(`words:${userId}:${roomId}`, count);
     }
 
     async getUsers() {
         if (!this.enabled) return [];
-        return await this.smembersSync('users');
+        return await this.client.smembers('users');
     }
 
     async getRooms() {
         if (!this.enabled) return [];
-        return await this.smembersSync('rooms');
+        return await this.client.smembers('rooms');
     }
 
     async getSentencesEntry(userId, roomId) {
         if (!this.enabled) return 0;
-        return Number(await this.getSync(`sentences:${userId}:${roomId}`));
+        return Number(await this.client.get(`sentences:${userId}:${roomId}`));
     }
 
     async getWordsEntry(userId, roomId) {
         if (!this.enabled) return 0;
-        return Number(await this.getSync(`words:${userId}:${roomId}`));
+        return Number(await this.client.get(`words:${userId}:${roomId}`));
     }
 
     async countSentencesByUserId(userId) {
         if (!this.enabled) return 0;
-        const keys = await this.keysSync(`sentences:${userId}:*`);
+        const keys = await this.client.keys(`sentences:${userId}:*`);
         let sum = 0;
         for (let key of keys) {
-            sum += Number(await this.getSync(key));
+            sum += Number(await this.client.get(key));
         }
         return sum;
     }
 
     async countWordsByUserId(userId) {
         if (!this.enabled) return 0;
-        const keys = await this.keysSync(`words:${userId}:*`);
+        const keys = await this.client.keys(`words:${userId}:*`);
         let sum = 0;
         for (let key of keys) {
-            sum += Number(await this.getSync(key));
+            sum += Number(await this.client.get(key));
         }
         return sum;
     }
 
     async countSentencesByRoomId(roomId) {
         if (!this.enabled) return 0;
-        const keys = await this.keysSync(`sentences:*:${roomId}`);
+        const keys = await this.client.keys(`sentences:*:${roomId}`);
         let sum = 0;
         for (let key of keys) {
-            sum += Number(await this.getSync(key));
+            sum += Number(await this.client.get(key));
         }
         return sum;
     }
 
     async countWordsByRoomId(roomId) {
         if (!this.enabled) return 0;
-        const keys = await this.keysSync(`words:*:${roomId}`);
+        const keys = await this.client.keys(`words:*:${roomId}`);
         let sum = 0;
         for (let key of keys) {
-            sum += Number(await this.getSync(key));
+            sum += Number(await this.client.get(key));
         }
         return sum;
     }
